e0e54f27ac20bfe98f755315b5213b3960f39828
yash mahajan <thecodereborn@gmail.com>
2016-10-02 21:25:23 +0530

    Mark the Answer

---
+++ HackerEarth/Arrays/Mark The Answer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	int n,x; cin >> n >> x;
+	int temp; int skip=1;
+	int count = 0;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin >> arr[i];
+		if(arr[i] <= x)
+			count++;
+		else{
+			if(skip != 1){
+		//		cout << " stop bit " << arr[i]<< endl;
+				break;
+			}
+			else{
+		//		cout << " skip bit " << arr[i]<< endl;
+			  skip--;
+			}
+		}
+	}	
+	cout << count << endl;
+}
